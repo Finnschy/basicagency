2010ba45036ed9f64c2d2599bb6c70b1e43471ff
Finnschy <skylerfearless@yahoo.de>
2020-12-24 18:04:40 +0100

    basics stehen

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require("express")
 const app = express()
+const navData = require("./nav.json")
 
 const PORT = process.env.PORT || 3000
 
 app.use(express.static("public"))
 app.set("view engine", "ejs")
 
-app.get("/", (req, res) => {
-    res.render("index")
+navData.forEach(navi => {
+    app.get(`${navi.url}`, (req, res) => {
+        res.render(`${navi.render}`, {title: `${navi.name}`, navData})
+    })
 })
+
 
 
 
